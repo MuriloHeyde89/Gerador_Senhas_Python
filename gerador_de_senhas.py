ed2f479b2b29166471a9f90213316a2bbe7e3e7f
Murilo Heyde <murilo.heyde89@gmail.com>
2022-10-16 12:26:03 -0300

    Iniciando o Gerador de Senhas

---
+++ gerador_de_senhas.py
@@ -0,0 +1,15 @@
+import random
+import PySimpleGUI as sg
+import os
+
+class GeradorSenha:
+    def __init__(self):
+        pass
+    def Iniciar(self):
+        pass
+    def salvar_senha(self):
+        pass
+
+gen = GeradorSenha()
+gen.Iniciar() 
+
