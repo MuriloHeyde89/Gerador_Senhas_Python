d84a4f03d6506af10c14fc5788fbdcdc581e86e8
Murilo Heyde <murilo.heyde89@gmail.com>
2022-10-17 14:49:36 -0300

    Continuidade do código até a criação grafica

--- gerador_de_senhas.py
+++ gerador_de_senhas.py
@@ -1,15 +1,34 @@
 import random
 import PySimpleGUI as sg
 import os
+from typing import List
 
-class GeradorSenha:
+class PassGen:
     def __init__(self):
-        pass
+        #Layout
+        sg.theme('Black')
+        layout = [
+            [sg.Text('Site/Software', size=(10, 1)),
+            sg.Input(key='site', size=(20, 1))],
+            [sg.Text('E-mail/Usuário', size=(10, 1)),
+            sg.Input(key='usuario', size=(20, 1))],
+            [sg.Text('Quantidade de caracteres'), sg.Combo(values=list(
+                range(30)),key='total_chars', default_value=1, size=(3, 1))],
+            [sg.Output(size=(32, 5))],
+            [sg.Button('Gerar Senha')]
+        ]
+        # Declarar janela
+        self.janela = sg.Window('Password Generator', layout)
+
     def Iniciar(self):
-        pass
+        while True:
+            evento, valores = self.janela.read()
+            if evento == sg.WINDOW_CLOSED:
+                break
+
     def salvar_senha(self):
         pass
 
-gen = GeradorSenha()
+gen = PassGen()
 gen.Iniciar() 
 
